998a0c811fa36c39c0a5ebd5979eb86697cb6e18
SidMandava <37759986+SidMandava@users.noreply.github.com>
2021-07-25 21:49:55 -0700

    Create RelayE1.31_V2.ino

---
+++ Pneumatic Controller/E131/RelayE1.31_V2.ino
@@ -0,0 +1,116 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <E131.h>
+
+//Ethernet Configuration
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+
+uint8_t ip[] = {192, 168, 2, 253};
+uint8_t subnet[] = {255, 255, 255, 0};
+uint8_t gateway[] = {192, 168, 2, 69};
+uint8_t dns[] = {0, 0, 0, 0};
+
+E131 e131;
+
+#define pins 18
+
+int ports[pins][2] = {
+
+  {1, 0},
+  {2, 1},
+  {3, 2},
+  {5, 3},
+  {6, 4},
+  {7, 5},
+  {8, 5},
+  {9, 6},
+  {30, 8},
+  {31, 9},
+  {32, 10},
+  {33, 11},
+  {34, 12},
+  {35, 13},
+  {36, 14},
+  {37, 15},
+  {38, 16},
+  {39, 17}
+
+};
+
+boolean active[pins];
+
+
+void setup() {
+
+  for (int i = 0; i < pins; i++) {
+    pinMode(ports[i][0], OUTPUT);
+    active[i] = false;
+  }
+
+
+  //Disable SD Card
+  pinMode(4, OUTPUT);
+  digitalWrite(4, HIGH);
+
+  //Set Up Printing
+  Serial.begin(9600);
+  Serial.println("");
+  Serial.println("Initialized Serial Printing (9600)");
+
+  //Initalize E1.31
+  e131.begin(mac, ip, gateway, subnet, dns);
+  Serial.println("\nSucsesfully Initialized E1.31\n");
+}
+
+void loop() {
+
+  //Parse A Packet
+  uint16_t num_channels = e131.parsePacket();
+  /*Serial.print("NUMCHANNELS: ");
+  Serial.println(num_channels);*/
+
+  //Print Channel Data
+  if (num_channels) {
+
+    Serial.print(F("Universe: "));
+    Serial.println(e131.universe);
+
+    Serial.print("Num Channels: ");
+    Serial.println(num_channels);
+
+    Serial.print("Errors: ");
+    Serial.println(e131.stats.packet_errors);
+
+    for (int i = 0; i < num_channels; i++) {
+      Serial.print("Channel ");
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.println(e131.data[i]);
+    }
+  }
+  else{
+    Serial.println("No E1.31 Data");
+  }
+  
+  Serial.println("");
+
+  for(int i=0; i<pins; i++){
+    if(e131.data[ports[i][1]] < 200){
+      active[i] = true;     
+    }
+    else{
+      active[i] = false;
+    }
+  }
+
+  for(int i=0; i<pins;i++){
+    if(active[i]){
+      digitalWrite(ports[i][0], HIGH);
+    }
+    else{
+      digitalWrite(ports[i][1], LOW);
+    }
+  }
+
+  delay(5);  
+}
